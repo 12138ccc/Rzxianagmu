1db650e01a9bd4f43f088f4288804f6ae4359403
hary <2339105817@qq.com>
2022-09-23 17:13:39 +0800

    feat:项目初始化

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,5 +5,18 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
+// 返回的方法具有一个 keys 属性
+// keys 函数 返回一个数组
+console.log(req)
+console.log(req.keys())
+// console.log(req('./dashboard.svg'))
+// const requireAll = requireContext => requireContext.keys().map(requireContext)
+const requireAll = requireContext => requireContext.keys().map((val) => {
+  return requireContext(val)
+})
+// map 返回一个新数组
+// 长和原来的一样
+// 映射
+// ['./dashboard.svg'].map(requireContext)
 requireAll(req)
+console.log(requireAll(req))
